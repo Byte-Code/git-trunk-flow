dda716cd5f9a0f7d058d0b32bb314058b291c191
Claudio Stella <cstella@byte-code.com>
2019-04-11 10:09:49 +0200

    Add fetch_all() before hc-branch and rc-branch aliases

--- bin/aliases/scripts/hc-branch.sh
+++ bin/aliases/scripts/hc-branch.sh
@@ -1,4 +1,5 @@
 . $PWD/trunk-flow/aliases/scripts/utils.sh
+fetch_all
 HC_BRANCH=$(get_last_hc_branch prod)
 if [ ${HC_BRANCH} ]; then
     echo "${HC_BRANCH}"
--- bin/aliases/scripts/rc-branch.sh
+++ bin/aliases/scripts/rc-branch.sh
@@ -1,4 +1,5 @@
 . $PWD/trunk-flow/aliases/scripts/utils.sh
+fetch_all
 RC_BRANCH=$(get_last_rc_branch prod)
 if [ ${RC_BRANCH} ]; then
     echo "${RC_BRANCH}"
