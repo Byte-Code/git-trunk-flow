9a270b2281ca19af27dd9005cbce5fd5b1d22f64
Claudio Stella <cstella@byte-code.com>
2019-07-18 12:50:33 +0200

    Add finish alias

---
+++ bin/aliases/scripts/finish.sh
@@ -0,0 +1,12 @@
+. $PWD/trunk-flow/aliases/scripts/utils.sh
+
+FIX=$(git refname)
+
+case "${FIX}" in
+    hotfix*) CMD="git finish-hotfix" ;;
+    *#*) CMD="git finish-rc-inhibit" ;;
+    *@*) CMD="git finish-rc-fix" ;;
+    *) CMD="git finish-fb" ;;
+esac
+
+echo "$CMD" && ${CMD}
