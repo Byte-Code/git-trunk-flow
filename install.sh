3b3965c79d442439fa05f2e347e818a753124cdf
Claudio Stella <cstella@byte-code.com>
2020-03-23 11:20:34 +0100

    Do not rely on svn anymore to extract bin/ into trunk-flow/

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ DEST=trunk-flow
 
 upgrade() {
     DEST=$1
-    svn export --force https://github.com/Byte-Code/git-trunk-flow/tags/${VERSION}/bin ${DEST} --non-interactive --trust-server-cert
+    ARCHIVE_BIN_PATH="git-trunk-flow-$(echo ${VERSION} | sed 's/^v//g')/bin/"
+    curl https://codeload.github.com/Byte-Code/git-trunk-flow/tar.gz/${VERSION} | tar -xvf - -C ./${DEST} --strip-components=2 ${ARCHIVE_BIN_PATH}
 }
 
 install() {
     DEST=$1
+    mkdir ${DEST}
     upgrade ${DEST}
     INCLUDE=$(cat << EOF
 [include]
